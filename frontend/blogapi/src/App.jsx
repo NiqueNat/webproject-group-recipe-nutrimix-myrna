bd1c0ebce797aadc21c034783f0ed145da5d032a
Zimo wu <jessica@Zimos-MacBook-Pro.local>
2024-03-24 19:40:53 -0400

    update set-up app.jsx
    Recipe detail

--- frontend/blogapi/src/App.jsx
+++ frontend/blogapi/src/App.jsx
@@ -1,5 +1,13 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
+import RecipeDetail from './page/RecipeDetail' // 导入 RecipeDetail 组件
+
+<Routes>
+  <Route index element={<Home />} />
+  <Route path="/home" element={<Home />} />
+  <Route path="/recipe-detail" element={<RecipeDetail />} /> {/* 添加新路径 */}
+</Routes>
+
 
 function App() {
   return (
---
+++ frontend/blogapi/src/pages/RecipeDetail.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../css/RecipeDtail.css'; 
+
+function RecipeDetail() {
+  return (
+    <div>
+      {/* 在这里添加显示食谱详细信息的 JSX 代码 */}
+      <h1>Recipe Detail Page</h1>
+    </div>
+  );
+}
+
+export default RecipeDetail;
